09e9f2ff4a08e7901e5d366180da05348d6dc38a
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-09-27 15:18:59 +0800

    Update boot.asm

--- os/boot.asm
+++ os/boot.asm
@@ -1,49 +1,6 @@
 ;this is 16 bit mode BIOS
 
-text: ;this function writes hello to the screen
-	mov ah, 0x0e ;set ah to tty mode
-	mov al,'k' ;set al to 'H'
-	int 0x10 ;call ah and process the data stored in al
-	mov al,'e'
-	int 0x10
-	mov al,'w'
-	int 0x10
-	mov al,'l'
-	int 0x10
-	mov al,'O'
-	int 0x10
-	mov al,'S'
-	int 0x10
-
-	mov al,0xa
-	int 0x10
-	mov al,0xd
-	int 0x10
-	
-	int 0x10
-	mov al,'H'
-	int 0x10
-	mov al,'e'
-	int 0x10
-	mov al,'l'
-	int 0x10
-	int 0x10
-	mov al,'o'
-	int 0x10
-	mov al,' '
-	int 0x10
-	mov al,'W'
-	int 0x10
-	mov al,'o'
-	int 0x10
-	mov al,'r'
-	int 0x10
-	mov al,'l'
-	int 0x10
-	mov al,'d'
-	int 0x10
-	mov al,'!'
-	int 0x10
+%include 'utils.asm' ;use the utils file
 
 mov dx,0 ; initalize main loop counter
 main: ;main loop
