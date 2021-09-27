337427d5009b4096f97b9b11f5e155a027fed7da
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-09-27 12:24:26 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,14 +4,8 @@
 # os/boot.asm is the bios boot file
 # bios-256k.bin efi-e1000.rom kvmvapic.bin vgabios-stdvga.bin are files needed by qemu but for some reason don't get installed with it
 
-if ! type "nasm" > /dev/null; then # install nasm if it doesn't exist
-	install-pkg nasm
-fi
-if ! type "qemu-system-x86_64" > /dev/null; then # install qemu if it doesn't exist
-	install-pkg qemu # qemu is the whole package which includes qemu functions
-fi
 
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:lib # add out lib folder to the library path
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:lib # add our lib folder to the library path
 
 nasm -f bin os/boot.asm -o boot.bin # compile os/boot.bin
 
