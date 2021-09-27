ce139c237432899cbf26e773bb7f352d09ef2c3b
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-09-27 09:33:46 +0800

    Add files via upload

---
+++ os/boot.asm
@@ -0,0 +1,27 @@
+;this is 16 bit mode BIOS
+
+hello: ;this function writes hello to the screen
+	mov ah, 0x0e ;set ah to tty mode
+	mov al,'H' ; set al to 'H'
+	int 0x10 ;call ah and process the data stored in al
+	mov al,'e'
+	int 0x10
+	mov al,'l'
+	int 0x10
+	int 0x10
+	mov al,'o'
+	int 0x10
+
+mov dx,0 ; initalize main loop counter
+main: ;main loop
+
+
+	cmp dx,1 ;check if dx = 1
+	jne main ; if !dx = 1 jump to main
+	add dx,1 ; increase counter
+	call hello ; get value from last compare and call if true
+	
+times 510-($-$$) db 0 ; fill rest of boot with '0'
+
+
+dw 0xaa55 ; some sort of magic number
