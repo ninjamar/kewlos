26bc0ade819f84f56d0b94c2182dd1a532055a89
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-09-28 08:14:49 +0800

    Update boot.asm

--- os/boot.asm
+++ os/boot.asm
@@ -1,15 +1,14 @@
 ;this is 16 bit mode BIOS
 
-%include 'utils.asm' ;use the utils file
+%include 'printlib.asm' ;use the utils file
 
 mov dx,0 ; initalize main loop counter
 main: ;main loop
-
-
 	cmp dx,1 ;check if dx = 1
 	jne main ; if !dx = 1 jump to main
 	add dx,1 ; increase counter
-	call text ; get value from last compare and call if true
+	mov si,"KewlOS"
+	call print_string
 	
 times 510-($-$$) db 0 ; fill rest of boot with '0'
 
