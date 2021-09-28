b6c926a1cbfb78e428512b0988ee1aa98f172ca8
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-09-28 08:14:05 +0800

    Create printlib.asm

---
+++ os/printlib.asm
@@ -0,0 +1,19 @@
+; this file contains printing utilities
+
+
+print_char:
+	mov	ah,	0x0E
+	int 0x10
+
+print_str:
+	lodsd
+	
+	cmp al,0
+	je .done
+	
+	call print_char
+	
+	jmp print_str
+
+.done:
+	ret
