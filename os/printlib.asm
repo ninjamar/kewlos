ffe3e0ba486ed512342787a093c41a3b205ca9a6
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-09-30 08:42:03 +0800

    Update printlib.asm

--- os/printlib.asm
+++ os/printlib.asm
@@ -1,18 +1,10 @@
-; this file contains printing utilities
-
-
-print_char:
-	mov ah,	0x0E
-	int 0x10
-
 print_str:
-	
-	lodsd
-	
-	cmp al,0
+	lodsb
+	cmp al,0x00
 	je .done
 	
-	call print_char
+	mov ah,0x0E
+	int 0x10
 	
 	jmp print_str
 
