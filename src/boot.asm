49833c2736f966ab094db6d20c902df757ecd475
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-10-03 09:16:46 +0800

    Update boot.asm

--- src/boot.asm
+++ src/boot.asm
@@ -1,17 +1,11 @@
 ;this is 16 bit mode BIOS
 [ORG 0x7C00]
 
-mov dx,1
 main:
 	mov si,msg
 	call print_str
+	jmp $
 	
-	jmp .loop
-
-.loop:
-	call get_keypress_echo
-	jmp .loop
-
 %include 'iolib.asm'
 msg db "KewlOS",0
 times 510-($-$$) db 0
